23a18e27b801dda44807e8eaf214374d5e000594
Ra225263 <aobak63@gmail.com>
2025-06-20 21:25:08 +0600

    some update

--- app/Repositories/BookRepository/BookRepositoryContact.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\BookRepository;
-
-use Illuminate\Database\Eloquent\Collection;
-
-interface BookRepositoryContact
-{
-}
--- app/Repositories/ChapterRepository/ChapterRepositoryContact.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace App\Repositories\ChapterRepository;
-
-use Illuminate\Database\Eloquent\Collection;
-
-interface ChapterRepositoryContact
-{
-}
